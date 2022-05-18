0215ea38ab4d6f2bdd2229a22892010c07ac44b4
YazheV1ka <89256514+YazheV1ka@users.noreply.github.com>
2022-05-18 20:24:03 +0300

    Add files via upload

---
+++ TheHumanGame/task/src/tictactoe/Button.java
@@ -0,0 +1,15 @@
+package tictactoe;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Button extends JButton {
+
+    public Button(String text) {
+        super(text);
+        this.setName("Button" + text);
+        this.setFont(new Font("Courier", Font.BOLD, 50));
+        this.setFocusPainted(false);
+    }
+
+}
